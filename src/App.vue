8dec78a129413d6fb9a769e2380bb54a4b10193b
Misha Grinko <glukym@gmail.com>
2024-08-24 15:52:23 +0300

    solution

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <script setup>
-// import movies from './data/movies.json';
+import { ref } from 'vue';
+import movies from './data/movies.json';
+
+const selectedMovie = ref(null);
 </script>
 
 <template>
@@ -7,58 +10,33 @@
   <div class="page">
     <div class="page-content">
       <div class="movies">
-        <div class="card has-background-grey" data-cy="movie">
+        <div
+          v-for="movie of movies"
+          :key="movie.imdbId"
+          data-cy="movie"
+          class="card"
+          :class="{
+            'has-background-grey': selectedMovie?.imdbId === movie.imdbId,
+          }"
+        >
           <header class="card-header">
-            <button class="card-header-icon" data-cy="movie__unselect-button">
+            <button
+              v-if="selectedMovie?.imdbId === movie.imdbId"
+              class="card-header-icon"
+              data-cy="movie__unselect-button"
+              @click="selectedMovie = null"
+            >
               <span class="icon">
                 <i class="fas fa-minus"></i>
               </span>
             </button>
-          </header>
 
-          <div class="card-image">
-            <figure class="image is-4by3">
-              <img
-                data-cy="movie__image"
-                src="https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg"
-                alt="Inception"
-              />
-            </figure>
-          </div>
-
-          <div class="card-content">
-            <div class="media">
-              <div class="media-left">
-                <figure class="image is-48x48">
-                  <img src="./assets/images/imdb-logo.jpeg" alt="imdb" />
-                </figure>
-              </div>
-
-              <div class="media-content">
-                <p class="title is-8" data-cy="movie__title">Inception</p>
-              </div>
-            </div>
-
-            <div class="content">
-              <p data-cy="movie__description">
-                Follows the lives of eight very different couples in dealing
-                with their love lives in various loosely interrelated tales all
-                set during a frantic month before Christmas in London, England.
-              </p>
-
-              <a
-                href="https://www.imdb.com/title/tt1375666"
-                data-cy="movie__link"
-              >
-                IMDB
-              </a>
-            </div>
-          </div>
-        </div>
-
-        <div class="card" data-cy="movie">
-          <header class="card-header">
-            <button class="card-header-icon" data-cy="movie__select-button">
+            <button
+              v-else
+              class="card-header-icon"
+              data-cy="movie__select-button"
+              @click="selectedMovie = movie"
+            >
               <span class="icon">
                 <i class="fas fa-plus"></i>
               </span>
@@ -69,57 +47,8 @@
             <figure class="image is-4by3">
               <img
                 data-cy="movie__image"
-                src="https://m.media-amazon.com/images/M/MV5BMTY4NjQ5NDc0Nl5BMl5BanBnXkFtZTYwNjk5NDM3._V1_.jpg"
-                alt="Love Actually"
-              />
-            </figure>
-          </div>
-
-          <div class="card-content">
-            <div class="media">
-              <div class="media-left">
-                <figure class="image is-48x48">
-                  <img src="./assets/images/imdb-logo.jpeg" alt="imdb" />
-                </figure>
-              </div>
-
-              <div class="media-content">
-                <p class="title is-8" data-cy="movie__title">Love Actually</p>
-              </div>
-            </div>
-
-            <div class="content">
-              <p data-cy="movie__description">
-                A thief who steals corporate secrets through the use of
-                dream-sharing technology is given the inverse task of planting
-                an idea into the mind of a C.E.O.
-              </p>
-
-              <a
-                href="https://www.imdb.com/title/tt0314331"
-                data-cy="movie__link"
-              >
-                IMDB
-              </a>
-            </div>
-          </div>
-        </div>
-
-        <div class="card" data-cy="movie">
-          <header class="card-header">
-            <button class="card-header-icon" data-cy="movie__select-button">
-              <span class="icon">
-                <i class="fas fa-plus"></i>
-              </span>
-            </button>
-          </header>
-
-          <div class="card-image">
-            <figure class="image is-4by3">
-              <img
-                data-cy="movie__image"
-                src="https://m.media-amazon.com/images/M/MV5BMTY4YWMzMTMtZjUyOS00OGY1LTljMGUtOWU5ZjYzMjc2ZTMwXkEyXkFqcGdeQXVyMTYzMDM0NTU@._V1_SY1000_CR0,0,701,1000_AL_.jpg"
-                alt="The Day After Tomorrow"
+                :src="movie.imgUrl"
+                :alt="movie.title"
               />
             </figure>
           </div>
@@ -134,35 +63,32 @@
 
               <div class="media-content">
                 <p class="title is-8" data-cy="movie__title">
-                  The Day After Tomorrow
+                  {{ movie.title }}
                 </p>
               </div>
             </div>
 
             <div class="content">
-              <p data-cy="movie__description">
-                Jack Hall, paleoclimatologist, must make a daring trek from
-                Washington, D.C. to New York City to reach his son, trapped in
-                the cross-hairs of a sudden international storm which plunges
-                the planet into a new Ice Age.
-              </p>
-
-              <a
-                href="https://www.imdb.com/title/tt0319262"
-                data-cy="movie__link"
-              >
-                IMDB
-              </a>
+              <p data-cy="movie__description">{{ movie.description }}</p>
+              <a :href="movie.imdbUrl" data-cy="movie__link">IMDB</a>
             </div>
           </div>
         </div>
       </div>
     </div>
 
-    <div class="sidebar" data-cy="Sidebar">
+    <div class="sidebar" data-cy="sidebar">
       <h2 class="subtitle">
-        <button data-cy="ClearButton" class="delete"></button>
-        <span data-cy="PageTitle">Inception</span>
+        <button
+          data-cy="clear-button"
+          class="delete"
+          v-if="selectedMovie"
+          @click="selectedMovie = null"
+        ></button>
+
+        <span data-cy="page-title">
+          {{ selectedMovie?.title || 'No movies selected' }}
+        </span>
       </h2>
     </div>
   </div>
